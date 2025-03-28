5d1ce2d0b2fc47e420ac9a581d92a13eea1fc7e5
Pedro Melo <pedromelocf@yahoo.com>
2025-03-27 21:12:12 -0300

    Connecting to db as dev with granted access only to the wp db.

--- srcs/requirements/mariadb/tools/dbscript.sh
+++ srcs/requirements/mariadb/tools/dbscript.sh
@@ -5,4 +5,20 @@ if [ "$(ls -la /var/lib/mysql | awk 'NR==1 {print $2}')" -eq 12 ]; then
 fi
 
 sudo service mariadb start
-sudo mariadb -u root -p"${MYSQL_ROOT_PASSWORD}";
+
+sudo mariadb -u root -p"${MYSQL_ROOT_PASSWORD}"
+
+sudo mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" -e "
+    CREATE DATABASE IF NOT EXISTS wp_db; 
+    CREATE ROLE IF NOT EXISTS developer; 
+    GRANT ALL PRIVILEGES ON wp_db.* TO developer; 
+    CREATE USER IF NOT EXISTS '""${MYSQL_USER}""'@'localhost';
+    ALTER USER '""${MYSQL_USER}""'@'localhost' IDENTIFIED BY '""${MYSQL_PASSWORD}""';
+    GRANT 'developer' TO '""${MYSQL_USER}""'@'localhost'; 
+    FLUSH PRIVILEGES;
+    SHOW GRANTS FOR '""${MYSQL_USER}""'@'localhost';
+    "
+
+sudo mariadb -u "${MYSQL_USER}" -p"${MYSQL_PASSWORD}" -e "SHOW DATABASES;"
+
+sudo mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" -e "SET ROLE 'developer'; SHOW DATABASES;"
