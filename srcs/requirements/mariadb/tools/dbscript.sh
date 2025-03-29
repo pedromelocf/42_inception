7b468cc0c032e8d015434d15b6cfc14ff38e031b
Pedro Melo <pedromelocf@yahoo.com>
2025-03-29 10:32:16 -0300

    Inserting file changes

--- srcs/requirements/mariadb/tools/dbscript.sh
+++ srcs/requirements/mariadb/tools/dbscript.sh
@@ -1,24 +1,21 @@
 #!/bin/sh
 
-if [ "$(ls -la /var/lib/mysql | awk 'NR==1 {print $2}')" -eq 12 ]; then
-    mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
-fi
+mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 
-sudo service mariadb start
+sleep 2;
 
-sudo mariadb -u root -p"${MYSQL_ROOT_PASSWORD}"
+/etc/init.d/mariadb start
 
-sudo mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" -e "
+sleep 2;
+
+mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" -e "
+    ALTER USER 'root'@'localhost' IDENTIFIED BY '"${MYSQL_ROOT_PASSWORD}"';
+    GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
     CREATE DATABASE IF NOT EXISTS wp_db; 
     CREATE ROLE IF NOT EXISTS developer; 
     GRANT ALL PRIVILEGES ON wp_db.* TO developer; 
-    CREATE USER IF NOT EXISTS '""${MYSQL_USER}""'@'localhost';
-    ALTER USER '""${MYSQL_USER}""'@'localhost' IDENTIFIED BY '""${MYSQL_PASSWORD}""';
-    GRANT 'developer' TO '""${MYSQL_USER}""'@'localhost'; 
+    CREATE USER IF NOT EXISTS '"${MYSQL_USER}"'@'localhost';
+    ALTER USER '"${MYSQL_USER}"'@'localhost' IDENTIFIED BY '"${MYSQL_PASSWORD}"';
+    GRANT 'developer' TO '"${MYSQL_USER}"'@'localhost'; 
     FLUSH PRIVILEGES;
-    SHOW GRANTS FOR '""${MYSQL_USER}""'@'localhost';
-    "
-
-sudo mariadb -u "${MYSQL_USER}" -p"${MYSQL_PASSWORD}" -e "SHOW DATABASES;"
-
-sudo mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" -e "SET ROLE 'developer'; SHOW DATABASES;"
+"
