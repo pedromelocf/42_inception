7849b478e8dd2d89deab79008194bdf634dc0ee4
Pedro Melo <pedromelocf@yahoo.com>
2025-04-01 22:23:12 -0300

    Fixing volume issue due to mount configuration; Implementing dockerfile to initialize services.

--- srcs/requirements/mariadb/tools/dbscript.sh
+++ srcs/requirements/mariadb/tools/dbscript.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
-
-sleep 2;
-
 /etc/init.d/mariadb start
 
 sleep 2;
@@ -11,9 +7,9 @@ sleep 2;
 mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" -e "
     ALTER USER 'root'@'localhost' IDENTIFIED BY '"${MYSQL_ROOT_PASSWORD}"';
     GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
-    CREATE DATABASE IF NOT EXISTS wp_db; 
+    CREATE DATABASE IF NOT EXISTS WordPress; 
     CREATE ROLE IF NOT EXISTS developer; 
-    GRANT ALL PRIVILEGES ON wp_db.* TO developer; 
+    GRANT ALL PRIVILEGES ON WordPress.* TO developer; 
     CREATE USER IF NOT EXISTS '"${MYSQL_USER}"'@'localhost';
     ALTER USER '"${MYSQL_USER}"'@'localhost' IDENTIFIED BY '"${MYSQL_PASSWORD}"';
     GRANT 'developer' TO '"${MYSQL_USER}"'@'localhost'; 
