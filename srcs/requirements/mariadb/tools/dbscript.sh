a9740880242205cb6ace9f3200fe1b430835bf95
Pedro Melo <pedromelocf@yahoo.com>
2025-04-03 13:35:06 -0300

    Implementing wpscript to install and configure wpdata; also fixing dbscript to allow connections from outside container with % users

--- srcs/requirements/mariadb/tools/dbscript.sh
+++ srcs/requirements/mariadb/tools/dbscript.sh
@@ -2,20 +2,30 @@
 
 /etc/init.d/mariadb start
 
-sleep 4;
+sleep 3;
 
 mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" -e "
+    ALTER USER 'root'@'localhost' IDENTIFIED BY '"${MYSQL_ROOT_PASSWORD}"';
     CREATE DATABASE IF NOT EXISTS WordPress; 
+
     CREATE ROLE IF NOT EXISTS developer; 
     GRANT ALL PRIVILEGES ON WordPress.* TO developer; 
+
     CREATE USER IF NOT EXISTS '"${MYSQL_USER}"'@'%';
     ALTER USER '"${MYSQL_USER}"'@'%' IDENTIFIED BY '"${MYSQL_PASSWORD}"';
     GRANT 'developer' TO '"${MYSQL_USER}"'@'%'; 
-    ALTER USER 'root'@'localhost' IDENTIFIED BY '"${MYSQL_ROOT_PASSWORD}"';
+
+    CREATE USER IF NOT EXISTS '"${MYSQL_USER}"'@'localhost';
+    ALTER USER '"${MYSQL_USER}"'@'localhost' IDENTIFIED BY '"${MYSQL_PASSWORD}"';
+    GRANT 'developer' TO '"${MYSQL_USER}"'@'localhost'; 
+
+    SET DEFAULT ROLE 'developer' FOR '"${MYSQL_USER}"'@'%';
+    SET DEFAULT ROLE 'developer' FOR '"${MYSQL_USER}"'@'localhost';
+
     FLUSH PRIVILEGES;
 "
 
-sleep 4;
+sleep 3;
 
 mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown;
 
--- srcs/requirements/wordpress/tools/wpscript.sh
+++ srcs/requirements/wordpress/tools/wpscript.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-if [ ! -f /srv/www/wordpress/wp-config.php ]; then
-  echo "Generating wp-config.php file. Connecting to mysql database:";
-  wp config create --path=/srv/www/wordpress --dbname=WordPress --dbuser=$WORDPRESS_MYSQL_USER --dbpass=$WORDPRESS_MYSQL_PASSWORD --dbhost=$WORDPRESS_MYSQL_HOST;
+sleep 10;
+cd /srv/www/wordpress;
 
-  if [ -f /srv/www/wordpress/wp-config.php ]; then
+if [ ! -f wp-config.php ]; then
+  echo "Generating wp-config.php file. Connecting to mysql database:";
+  wp config create --dbname=WordPress --dbuser=$WORDPRESS_MYSQL_USER --dbpass=$WORDPRESS_MYSQL_PASSWORD --dbhost=$WORDPRESS_MYSQL_HOST --allow-root;
+
+  if [ -f wp-config.php ]; then
     echo "wp-config.php generated succefully.";
   else
     echo "Generating wp-config.php failed. Check DB connection";
@@ -13,3 +16,19 @@ if [ ! -f /srv/www/wordpress/wp-config.php ]; then
 else
   echo "wp-config.php file already exists";
 fi
+
+if ! wp core is-installed --allow-root; then
+  echo "Installing wp core.";
+  wp core install --url=wpclidemo.dev --title="WP-CLI" --admin_user=$WORDPRESS_USER --admin_password=$WORDPRESS_PASSWORD --admin_email=info@wp-cli.org --allow-root; 
+
+  if wp core is-installed --allow-root; then
+    echo "Wp core installed succefully.";
+  else
+    echo "Wp core installing failed."
+    exit 2;
+  fi
+else
+  echo "wp core already installed";
+fi
+
+wp plugin update --all --allow-root;
